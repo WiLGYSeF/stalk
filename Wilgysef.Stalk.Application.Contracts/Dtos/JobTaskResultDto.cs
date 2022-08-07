43ae6e1603c4819b3b257a1a2a1d27661f3eeaee
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-08-06 20:56:16 -0400

    update tests

--- Wilgysef.Stalk.Application.Contracts/Dtos/JobTaskResultDto.cs
+++ Wilgysef.Stalk.Application.Contracts/Dtos/JobTaskResultDto.cs
@@ -2,11 +2,11 @@
 
 public class JobTaskResultDto
 {
+    public bool Success { get; set; }
+
     public string? ErrorCode { get; set; }
 
     public string? ErrorMessage { get; set; }
 
     public string? ErrorDetail { get; set; }
-
-    public bool IsSuccess { get; set; }
 }
