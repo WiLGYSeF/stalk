529326e5331a9a2f874f2223f182d9e2f6bd1635
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-08-09 22:17:08 -0400

    update handler

---
+++ Wilgysef.Stalk.Core/DomainEvents/Handlers/JobEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Wilgysef.Stalk.Core.DomainEvents.Events;
+
+namespace Wilgysef.Stalk.Core.DomainEvents.Handlers;
+
+public class JobEventHandler :
+    IDomainEventHandler<JobCreatedEvent>,
+    IDomainEventHandler<JobStateChangedEvent>,
+    IDomainEventHandler<JobPriorityChangedEvent>
+{
+    public async Task HandleEventAsync(JobCreatedEvent eventData)
+    {
+        await WorkPrioritizedJobs();
+    }
+
+    public async Task HandleEventAsync(JobStateChangedEvent eventData)
+    {
+        await WorkPrioritizedJobs();
+    }
+
+    public async Task HandleEventAsync(JobPriorityChangedEvent eventData)
+    {
+        await WorkPrioritizedJobs();
+    }
+
+    private async Task WorkPrioritizedJobs()
+    {
+        // TODO: start background job
+    }
+}
