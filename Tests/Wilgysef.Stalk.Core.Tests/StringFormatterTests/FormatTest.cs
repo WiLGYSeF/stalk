9f00db9989e59b690747600943c4f69141a551bb
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-08-20 15:41:14 -0400

    add formatter

---
+++ Tests/Wilgysef.Stalk.Core.Tests/StringFormatterTests/FormatTest.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using Wilgysef.Stalk.Core.StringFormatters;
+
+namespace Wilgysef.Stalk.Core.Tests.StringFormatterTests;
+
+public class FormatTest
+{
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("abc test", "abc test")]
+    [InlineData("{", "{")]
+    [InlineData("}", "}")]
+    [InlineData("{}", "")]
+    [InlineData("{{}}", "{}}")]
+    [InlineData("{{}", "{}")]
+    [InlineData("{}}", "}")]
+    [InlineData("{{{}", "{")]
+    [InlineData("{{{{}", "{{}")]
+    [InlineData("{{{{{}", "{{")]
+    [InlineData("d{{asdf}b", "d{asdf}b")]
+    public void Format_Strings(string format, string expected)
+    {
+        new StringFormatter().Format(format, new Dictionary<string, object>()).ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("-{test}-", "-abc-")]
+    [InlineData("-{notexist}-", "--")]
+    [InlineData("-{notexist|test}-", "-abc-")]
+    [InlineData("-{notexist|notexist}-", "--")]
+    [InlineData("-{notexist|\"test}-", "-test-")]
+    [InlineData("-{notexist|\"test\"}-", "-test-")]
+    [InlineData("-{test}-{notexist}-{num}-", "-abc--123-")]
+    [InlineData("-{test,5}-", "-  abc-")]
+    [InlineData("-{test,-5}-", "-abc  -")]
+    [InlineData("-{test,2}-", "-abc-")]
+    [InlineData("-{test,-2}-", "-abc-")]
+    public void Format_Strings_Replace(string format, string expected)
+    {
+        var replace = new Dictionary<string, object>
+        {
+            { "test", "abc" },
+            { "num", 123 },
+        };
+
+        new StringFormatter().Format(format, replace).ShouldBe(expected);
+    }
+}
---
+++ Wilgysef.Stalk.Core/StringFormatters/StringFormatter.cs
@@ -0,0 +1,157 @@
+﻿using System.Text;
+using Wilgysef.Stalk.Core.Shared.StringFormatters;
+using Wilgysef.Stalk.Core.StringFormatters.FormatTokens;
+
+namespace Wilgysef.Stalk.Core.StringFormatters;
+
+public class StringFormatter : IStringFormatter
+{
+    public string Format(string value, IDictionary<string, object> formatValues)
+    {
+        var tokenizer = new FormatTokenizer();
+        using var tokenEnumerator = tokenizer.GetTokens(value).GetEnumerator();
+
+        var builder = new StringBuilder();
+        FormatToken? lastToken = null;
+        var inFormat = false;
+        
+        while (tokenEnumerator.MoveNext())
+        {
+            var token = tokenEnumerator.Current;
+
+            switch (token.Type)
+            {
+                case FormatTokenType.Constant:
+                    if (inFormat)
+                    {
+                        builder.Append(FormatFormatter(tokenizer, token.Value, formatValues));
+                    }
+                    else
+                    {
+                        builder.Append(token.Value);
+                    }
+                    break;
+                case FormatTokenType.FormatBegin:
+                    if (lastToken?.Type == FormatTokenType.FormatBegin)
+                    {
+                        inFormat = !inFormat;
+                        if (!inFormat)
+                        {
+                            builder.Append('{');
+                        }
+                    }
+                    else
+                    {
+                        inFormat = true;
+                    }
+                    break;
+                case FormatTokenType.FormatEnd:
+                    if (!inFormat)
+                    {
+                        // TODO: do not write double close braces
+                        builder.Append('}');
+                    }
+                    inFormat = false;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            lastToken = token;
+        }
+
+        if (lastToken?.Type == FormatTokenType.FormatBegin)
+        {
+            builder.Append('{');
+        }
+
+        return builder.ToString();
+    }
+
+    private string FormatFormatter(FormatTokenizer tokenizer, string value, IDictionary<string, object> formatValues)
+    {
+        var tokens = tokenizer.GetFormatTokens(value);
+        object? result = null;
+        var alignment = GetFormatTokenValue(tokens, FormatTokenType.FormatAlignment, out _);
+        var formatter = GetFormatTokenValue(tokens, FormatTokenType.FormatFormatter, out _);
+        var defaultValue = GetFormatDefaultTokenValue(tokens, formatValues, out _);
+
+        if (tokens.Count > 0 && tokens[0].Type == FormatTokenType.Constant)
+        {
+            formatValues.TryGetValue(tokens[0].Value, out result);
+        }
+
+        string resultValue;
+        if (formatter != null)
+        {
+            throw new NotImplementedException();
+        }
+        else
+        {
+            resultValue = (result ?? "").ToString() ?? "";
+        }
+
+        if (result == null && defaultValue != null)
+        {
+            resultValue = defaultValue;
+        }
+
+        if (alignment != null)
+        {
+            if (int.TryParse(alignment, out var alignmentValue))
+            {
+                resultValue = AlignString(resultValue, alignmentValue);
+            }
+        }
+
+        return resultValue;
+    }
+
+    private string? GetFormatTokenValue(IList<FormatToken> tokens, FormatTokenType type, out int index)
+    {
+        string? result = null;
+        index = -1;
+
+        for (var i = 0; i < tokens.Count; i++)
+        {
+            if (tokens[i].Type != type)
+            {
+                continue;
+            }
+
+            index = i + 1;
+            if (i < tokens.Count - 1 && tokens[i + 1].Type == FormatTokenType.Constant)
+            {
+                result = tokens[i + 1].Value;
+            }
+        }
+
+        return result;
+    }
+
+    private string? GetFormatDefaultTokenValue(IList<FormatToken> tokens, IDictionary<string, object> formatValues, out int index)
+    {
+        var result = GetFormatTokenValue(tokens, FormatTokenType.FormatDefault, out index);
+        if (index == -1 || result == null)
+        {
+            return null;
+        }
+
+        if (result.StartsWith('"'))
+        {
+            result = result[1..];
+            return result[^1] == '"' ? result[..^1] : result;
+        }
+        if (!formatValues.TryGetValue(result, out var resultValue))
+        {
+            return null;
+        }
+        return resultValue.ToString();
+    }
+
+    private string AlignString(string value, int alignment)
+    {
+        var alignmentAbs = Math.Abs(alignment);
+        return alignment > 0 ? value.PadLeft(alignmentAbs) : value.PadRight(alignmentAbs);
+    }
+}
