807e546d9989a7a900c57549994193168a1ece7a
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-09-05 11:21:31 -0400

    remove unused

--- Wilgysef.Stalk.Core/JobTaskWorkerServices/IJobTaskWorkerService.cs
+++ Wilgysef.Stalk.Core/JobTaskWorkerServices/IJobTaskWorkerService.cs
@@ -1,5 +1,4 @@
-﻿using Wilgysef.Stalk.Core.Models.Jobs;
-using Wilgysef.Stalk.Core.Models.JobTasks;
+﻿using Wilgysef.Stalk.Core.Models.JobTasks;
 using Wilgysef.Stalk.Core.Shared.Dependencies;
 
 namespace Wilgysef.Stalk.Core.JobTaskWorkerServices;
