ad60bfe3176a2fbb629d43c971818a9425baf73d
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-09-06 00:32:11 -0400

    add base test

---
+++ Tests/Wilgysef.Stalk.WebApi.Tests/WebApiBaseTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Wilgysef.Stalk.WebApi.Tests;
+
+public abstract class WebApiBaseTest
+{
+    public WebApplicationFactory<Program> WebApplicationFactory { get; }
+    public HttpClient Client { get; }
+
+    public WebApiBaseTest()
+    {
+        WebApplicationFactory = new WebApiFactory().CreateApplication();
+        Client = WebApplicationFactory.CreateClient();
+    }
+}
