4ed5c4ffab0e59635818bbd33338b5a4d68a94cb
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-09-05 20:41:06 -0400

    add missing files

---
+++ Wilgysef.Stalk.Application/AssemblyLoaders/AssemblyLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Wilgysef.Stalk.Application.AssemblyLoaders;
+
+internal static class AssemblyLoader
+{
+    public static List<Assembly> LoadAssemblies(string path)
+    {
+        var assemblies = new List<Assembly>();
+        foreach (var file in Directory.GetFiles(path))
+        {
+            try
+            {
+                assemblies.Add(Assembly.LoadFile(file));
+            }
+            catch (BadImageFormatException) { }
+        }
+        return assemblies;
+    }
+}
