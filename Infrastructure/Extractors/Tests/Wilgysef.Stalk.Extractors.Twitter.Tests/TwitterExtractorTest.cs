72589bdb1ffc72896b9c7fb4015adc20ebda7cae
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-10-16 16:36:35 -0400

    fix tests

--- Infrastructure/Extractors/Tests/Wilgysef.Stalk.Extractors.Twitter.Tests/TwitterExtractorTest.cs
+++ Infrastructure/Extractors/Tests/Wilgysef.Stalk.Extractors.Twitter.Tests/TwitterExtractorTest.cs
@@ -146,7 +146,7 @@ public class TwitterExtractorTest : BaseTest
         result.Uri.AbsoluteUri.ShouldBe("https://pbs.twimg.com/media/FabmmiTaIAEO4zM?format=jpg&name=large");
         result.Metadata!["created_at"].ShouldBe("2022-08-18 08:52:30");
         result.Metadata["favorite_count"].ShouldBe(17340);
-        result.Metadata["file.extension"].ShouldBe("jpg");
+        result.Metadata["file", "extension"].ShouldBe("jpg");
         result.Metadata["is_quote_status"].ShouldBe(false);
         result.Metadata["lang"].ShouldBe("zxx");
         result.Metadata["media_id"].ShouldBe("1560187870648082433");
@@ -155,8 +155,8 @@ public class TwitterExtractorTest : BaseTest
         result.Metadata["reply_count"].ShouldBe(103);
         result.Metadata["retweet_count"].ShouldBe(1114);
         result.Metadata["tweet_id"].ShouldBe("1560187874460733440");
-        result.Metadata["user.id"].ShouldBe("1308334634745249793");
-        result.Metadata["user.screen_name"].ShouldBe("amatsukauto");
+        result.Metadata["user", "id"].ShouldBe("1308334634745249793");
+        result.Metadata["user", "screen_name"].ShouldBe("amatsukauto");
         result.Type.ShouldBe(JobTaskType.Download);
     }
 
@@ -174,17 +174,17 @@ public class TwitterExtractorTest : BaseTest
         result.Uri.AbsoluteUri.ShouldBe("data:text/plain;charset=UTF-8;base64,U3BsYXRvb24yIOOBj+OCszrlvaEgaHR0cHM6Ly90LmNvL0VXYkJQVG1FNEwKaHR0cHM6Ly93d3cudHdpdGNoLnR2L3V0b255YW4=");
         result.Metadata!["created_at"].ShouldBe("2022-08-03 04:09:30");
         result.Metadata["favorite_count"].ShouldBe(2022);
-        result.Metadata["file.extension"].ShouldBe("txt");
+        result.Metadata["file", "extension"].ShouldBe("txt");
         result.Metadata["is_quote_status"].ShouldBe(false);
         result.Metadata["lang"].ShouldBe("ja");
-        result.Metadata["origin.uri"].ShouldBe("https://twitter.com/amatsukauto/status/1554680837861683200");
+        result.Metadata["origin", "uri"].ShouldBe("https://twitter.com/amatsukauto/status/1554680837861683200");
         result.Metadata["possibly_sensitive"].ShouldBe(false);
         result.Metadata["quote_count"].ShouldBe(0);
         result.Metadata["reply_count"].ShouldBe(17);
         result.Metadata["retweet_count"].ShouldBe(129);
         result.Metadata["tweet_id"].ShouldBe("1554680837861683200");
-        result.Metadata["user.id"].ShouldBe("1308334634745249793");
-        result.Metadata["user.screen_name"].ShouldBe("amatsukauto");
+        result.Metadata["user", "id"].ShouldBe("1308334634745249793");
+        result.Metadata["user", "screen_name"].ShouldBe("amatsukauto");
         result.Type.ShouldBe(JobTaskType.Download);
     }
 
@@ -202,17 +202,17 @@ public class TwitterExtractorTest : BaseTest
         textResult.Uri.AbsoluteUri.ShouldBe("data:text/plain;charset=UTF-8;base64,5paw44GX44GE44Kr44OQ44O844KS5oqV56i/44GX44G+44GX44Gf4p2VCuOBi+OBo+OBk+OBhOOBhOOBruOBp+OBn+OBj+OBleOCk+iBnuOBhOOBpuOBj+OBoOOBleOBhOODvO+8geKZoQpmdWxs77yaaHR0cHM6Ly90LmNvL25sTE9ZbENtbE0KClZvY2Fs77ya44OK44OK44Kr44Kw44OpLCDlpKnkvb/jgYbjgagKSWxsdXN077yaSmFueWhlcm8gICDmp5gKTWl477yaUGQuNDbjgIDmp5gKTW92aWXvvJpSaWVzeiAg5qeYCmh0dHBzOi8veW91dHUuYmUvSEJrdExUeUxMOVU=");
         textResult.Metadata!["created_at"].ShouldBe("2022-05-08 12:20:00");
         textResult.Metadata["favorite_count"].ShouldBe(5823);
-        textResult.Metadata["file.extension"].ShouldBe("txt");
+        textResult.Metadata["file", "extension"].ShouldBe("txt");
         textResult.Metadata["is_quote_status"].ShouldBe(false);
         textResult.Metadata["lang"].ShouldBe("ja");
-        textResult.Metadata["origin.uri"].ShouldBe("https://twitter.com/amatsukauto/status/1523276529123397632");
+        textResult.Metadata["origin", "uri"].ShouldBe("https://twitter.com/amatsukauto/status/1523276529123397632");
         textResult.Metadata["possibly_sensitive"].ShouldBe(false);
         textResult.Metadata["quote_count"].ShouldBe(33);
         textResult.Metadata["reply_count"].ShouldBe(116);
         textResult.Metadata["retweet_count"].ShouldBe(912);
         textResult.Metadata["tweet_id"].ShouldBe("1523276529123397632");
-        textResult.Metadata["user.id"].ShouldBe("1308334634745249793");
-        textResult.Metadata["user.screen_name"].ShouldBe("amatsukauto");
+        textResult.Metadata["user", "id"].ShouldBe("1308334634745249793");
+        textResult.Metadata["user", "screen_name"].ShouldBe("amatsukauto");
         textResult.Type.ShouldBe(JobTaskType.Download);
 
         var thumbnailResult = results.Single(r => r.Uri.AbsoluteUri.StartsWith("https://pbs.twimg.com/ext_tw_video_thumb"));
@@ -220,7 +220,7 @@ public class TwitterExtractorTest : BaseTest
         thumbnailResult.Uri.AbsoluteUri.ShouldBe("https://pbs.twimg.com/ext_tw_video_thumb/1523196911448035328/pu/img/IjK77EYau0_-qDCu.jpg");
         thumbnailResult.Metadata!["created_at"].ShouldBe("2022-05-08 12:20:00");
         thumbnailResult.Metadata["favorite_count"].ShouldBe(5823);
-        thumbnailResult.Metadata["file.extension"].ShouldBe("jpg");
+        thumbnailResult.Metadata["file", "extension"].ShouldBe("jpg");
         thumbnailResult.Metadata["is_quote_status"].ShouldBe(false);
         thumbnailResult.Metadata["lang"].ShouldBe("ja");
         thumbnailResult.Metadata["media_id"].ShouldBe("1523196911448035328");
@@ -229,8 +229,8 @@ public class TwitterExtractorTest : BaseTest
         thumbnailResult.Metadata["reply_count"].ShouldBe(116);
         thumbnailResult.Metadata["retweet_count"].ShouldBe(912);
         thumbnailResult.Metadata["tweet_id"].ShouldBe("1523276529123397632");
-        thumbnailResult.Metadata["user.id"].ShouldBe("1308334634745249793");
-        thumbnailResult.Metadata["user.screen_name"].ShouldBe("amatsukauto");
+        thumbnailResult.Metadata["user", "id"].ShouldBe("1308334634745249793");
+        thumbnailResult.Metadata["user", "screen_name"].ShouldBe("amatsukauto");
         thumbnailResult.Type.ShouldBe(JobTaskType.Download);
 
         var videoResult = results.Single(r => r.Uri.AbsoluteUri.StartsWith("https://video.twimg.com/ext_tw_video/"));
@@ -238,7 +238,7 @@ public class TwitterExtractorTest : BaseTest
         videoResult.Uri.AbsoluteUri.ShouldBe("https://video.twimg.com/ext_tw_video/1523196911448035328/pu/vid/1280x720/r-Ybk23JsBkJIy9b.mp4?tag=12");
         videoResult.Metadata!["created_at"].ShouldBe("2022-05-08 12:20:00");
         videoResult.Metadata["favorite_count"].ShouldBe(5823);
-        videoResult.Metadata["file.extension"].ShouldBe("mp4");
+        videoResult.Metadata["file", "extension"].ShouldBe("mp4");
         videoResult.Metadata["is_quote_status"].ShouldBe(false);
         videoResult.Metadata["lang"].ShouldBe("ja");
         videoResult.Metadata["media_id"].ShouldBe("1523196911448035328");
@@ -247,11 +247,11 @@ public class TwitterExtractorTest : BaseTest
         videoResult.Metadata["reply_count"].ShouldBe(116);
         videoResult.Metadata["retweet_count"].ShouldBe(912);
         videoResult.Metadata["tweet_id"].ShouldBe("1523276529123397632");
-        videoResult.Metadata["user.id"].ShouldBe("1308334634745249793");
-        videoResult.Metadata["user.screen_name"].ShouldBe("amatsukauto");
-        videoResult.Metadata["video.bitrate"].ShouldBe(2176000);
-        videoResult.Metadata["video.duration_millis"].ShouldBe(26541);
-        videoResult.Metadata["video.view_count"].ShouldBe(76821);
+        videoResult.Metadata["user", "id"].ShouldBe("1308334634745249793");
+        videoResult.Metadata["user", "screen_name"].ShouldBe("amatsukauto");
+        videoResult.Metadata["video", "bitrate"].ShouldBe(2176000);
+        videoResult.Metadata["video", "duration_millis"].ShouldBe(26541);
+        videoResult.Metadata["video", "view_count"].ShouldBe(76821);
         videoResult.Type.ShouldBe(JobTaskType.Download);
     }
 
@@ -269,20 +269,20 @@ public class TwitterExtractorTest : BaseTest
         result.Uri.AbsoluteUri.ShouldBe("data:text/plain;charset=UTF-8;base64,UlQgQEFzdGVyQXJjYWRpYTogRElWSU5FIERVTyBUQUtJTkcgT1ZFUiBBUEVYIExFR0VORFMhCkdhbWluZyB3LyDimIHvuI9AYW1hdHN1a2F1dG8gCgrwn5KraHR0cHM6Ly90LmNvL0NXZnhtMFlVTXggaHR0cHM6Ly90LmNvL0tYTnB5THhQeQpodHRwOi8veW91dHUuYmUvM0Vobm1rWjhPQlE=");
         result.Metadata!["created_at"].ShouldBe("2022-09-08 01:03:48");
         result.Metadata["favorite_count"].ShouldBe(0);
-        result.Metadata["file.extension"].ShouldBe("txt");
+        result.Metadata["file", "extension"].ShouldBe("txt");
         result.Metadata["is_quote_status"].ShouldBe(false);
         result.Metadata["lang"].ShouldBe("en");
-        result.Metadata["origin.uri"].ShouldBe("https://twitter.com/amatsukauto/status/1567680068113285121");
+        result.Metadata["origin", "uri"].ShouldBe("https://twitter.com/amatsukauto/status/1567680068113285121");
         result.Metadata["possibly_sensitive"].ShouldBe(false);
         result.Metadata["quote_count"].ShouldBe(0);
         result.Metadata["reply_count"].ShouldBe(0);
         result.Metadata["retweet_count"].ShouldBe(257);
-        result.Metadata["retweet.tweet_id"].ShouldBe("1567677602781069314");
-        result.Metadata["retweet.user.id"].ShouldBe("1545352592884084736");
-        result.Metadata["retweet.user.screen_name"].ShouldBe("AsterArcadia");
+        result.Metadata["retweet", "tweet_id"].ShouldBe("1567677602781069314");
+        result.Metadata["retweet", "user", "id"].ShouldBe("1545352592884084736");
+        result.Metadata["retweet", "user", "screen_name"].ShouldBe("AsterArcadia");
         result.Metadata["tweet_id"].ShouldBe("1567680068113285121");
-        result.Metadata["user.id"].ShouldBe("1308334634745249793");
-        result.Metadata["user.screen_name"].ShouldBe("amatsukauto");
+        result.Metadata["user", "id"].ShouldBe("1308334634745249793");
+        result.Metadata["user", "screen_name"].ShouldBe("amatsukauto");
         result.Type.ShouldBe(JobTaskType.Download);
     }
 
